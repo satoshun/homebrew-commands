5dccebaf8072cd749735211df7fc4e676b674087
SatoShun <shun.sato1@gmail.com>
2014-05-06 12:22:20 +0900

    latest sha1

--- pull-request.rb
+++ pull-request.rb
@@ -3,7 +3,7 @@ require 'formula'
 class PullRequest < GithubGistFormula
   homepage 'https://github.com/satoshun/commands'
   url 'https://raw.githubusercontent.com/satoshun/commands/master/pull_request'
-  sha1 '10c6c5afda0a26cac05ad7765102f0c6edd68125'
+  sha1 'c1d30c29915a86751fc86ad7250627ec02459db4'
 
   def install
     bin.install 'pull_request'
