0179598886508df932b88821e7c2e37d3cb177f6
SatoShun <shun.sato1@gmail.com>
2014-05-07 20:53:49 +0900

    use sha1

--- sha1.rb
+++ sha1.rb
@@ -3,6 +3,7 @@ require 'formula'
 class Sha1 < GithubGistFormula
   homepage 'https://github.com/satoshun/commands'
   url 'https://raw.githubusercontent.com/satoshun/commands/master/sha1'
+  sha1 '3c959df64feb2420ffda05d8b1295e1ddb12bf46'
 
   def install
     bin.install 'sha1'
