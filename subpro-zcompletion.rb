84500fc0948b24efd445c2ad8133fb85f6bf098a
SatoShun <shun.sato1@gmail.com>
2014-06-01 07:30:15 +0000

    fix class name

--- subpro-zcompletion.rb
+++ subpro-zcompletion.rb
@@ -1,6 +1,6 @@
 require 'formula'
 
-class SubproCompletion < GithubGistFormula
+class SubproZcompletion < GithubGistFormula
   homepage 'https://github.com/satoshun/subpro'
   url 'https://raw.githubusercontent.com/satoshun/subpro/v1.1.0/subpro_zcompletion'
   sha1 '730fe262c59d5327715d2d28c4c5c7851ebcf05b'
