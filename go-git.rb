b482d3030c24991b522a394e4e23fc2ea4ff733a
SatoShun <shun.sato1@gmail.com>
2014-08-29 13:28:42 +0000

    go-git: update v0.0.2

--- go-git.rb
+++ go-git.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class GoGit < Formula
   homepage 'https://github.com/satoshun/go-git'
-  version 'v0.0.1'
-  url 'https://raw.githubusercontent.com/satoshun/go-git/v0.0.1/pkg/darwin/go-git'
-  sha1 'f6761c8a393d58e1392dec6ee6611e29f61e2004'
+  version 'v0.0.2'
+  url 'https://raw.githubusercontent.com/satoshun/go-git/v0.0.2/pkg/darwin/go-git'
+  sha1 '59df0c5832d987760dfa19373213a6d8a026f5bd'
 
   def install
     bin.install 'go-git'
