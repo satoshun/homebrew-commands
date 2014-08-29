c956ecff3e6da20cef00f6fa4c17ceb4d48bca51
SatoShun <shun.sato1@gmail.com>
2014-08-29 12:38:30 +0000

    go-git: fix endpoint

--- go-git.rb
+++ go-git.rb
@@ -3,7 +3,7 @@ require 'formula'
 class GoGit < Formula
   homepage 'https://github.com/satoshun/go-git'
   version 'v0.0.1'
-  url 'https://raw.githubusercontent.com/satoshun/go-git/v0.0.1/pkg/go-git_darwin'
+  url 'https://raw.githubusercontent.com/satoshun/go-git/v0.0.1/pkg/darwin/go-git'
   sha1 'f6761c8a393d58e1392dec6ee6611e29f61e2004'
 
   def install
