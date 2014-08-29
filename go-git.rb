49eb175e808ed7ed629f606eaafbbfe7924dfb16
SatoShun <shun.sato1@gmail.com>
2014-08-29 14:19:20 +0000

    go-git: udpate v0.0.3

--- go-git.rb
+++ go-git.rb
@@ -3,8 +3,8 @@ require 'formula'
 class GoGit < Formula
   homepage 'https://github.com/satoshun/go-git'
   version 'v0.0.2'
-  url 'https://raw.githubusercontent.com/satoshun/go-git/v0.0.2/pkg/darwin/go-git'
-  sha1 '59df0c5832d987760dfa19373213a6d8a026f5bd'
+  url 'https://raw.githubusercontent.com/satoshun/go-git/v0.0.3/pkg/darwin/go-git'
+  sha1 'c3a6c1893749b3ac92480841bbcd4c43bca0ab92'
 
   def install
     bin.install 'go-git'
