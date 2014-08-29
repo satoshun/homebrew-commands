af5bd4b3797a0bd0aec984dc8fb8e71ead4cd958
SatoShun <shun.sato1@gmail.com>
2014-08-29 14:23:08 +0000

    oops: update v0.0.3

--- go-git.rb
+++ go-git.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class GoGit < Formula
   homepage 'https://github.com/satoshun/go-git'
-  version 'v0.0.2'
+  version 'v0.0.3'
   url 'https://raw.githubusercontent.com/satoshun/go-git/v0.0.3/pkg/darwin/go-git'
   sha1 'c3a6c1893749b3ac92480841bbcd4c43bca0ab92'
 
