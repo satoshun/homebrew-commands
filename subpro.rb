e4b9c2d6b51cb51f831ada9dfddd9325f96752c6
SatoShun <shun.sato1@gmail.com>
2014-08-30 07:54:26 +0000

    subpro: update v2.0.0

--- subpro.rb
+++ subpro.rb
@@ -2,16 +2,13 @@ require 'formula'
 
 class Subpro < GithubGistFormula
   homepage 'https://github.com/satoshun/subpro'
-  url 'https://raw.githubusercontent.com/satoshun/subpro/v1.0.3/subpro'
-  sha1 '2a423f3dabdf50930994d6c9f3b15a73a6b58617'
+  version 'v2.0.0'
+  url 'https://raw.githubusercontent.com/satoshun/subpro/v2.0.0/pkg/darwin/subpro'
+  sha1 '9bc950cbfab26c5ededfc7be335a6e0c86da15d7'
 
   def install
     bin.install 'subpro'
     system 'mkdir -p ~/.subpro/base'
     system 'curl -o ~/.subpro/base/base.sublime-project https://raw.githubusercontent.com/satoshun/subpro/master/base.sublime-project'
-  end
-
-  def test
-    system 'subpro'
   end
 end
