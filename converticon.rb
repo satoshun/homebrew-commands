abbbf85309cad7f12f2ba8ae46c5d2998443a8fb
SatoShun <shun.sato1@gmail.com>
2014-10-26 05:32:36 +0000

    update converticon

--- converticon.rb
+++ converticon.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Converticon < Formula
   homepage 'https://github.com/satoshun/android_icon'
-  version 'v0.1'
-  url 'https://raw.githubusercontent.com/satoshun/android_icon/v0.1/pkg/darwin/converticon'
-  sha1 '8ca35ea75a8c9d49b1ee047718919743b0481a3b'
+  version 'v0.2'
+  url 'https://raw.githubusercontent.com/satoshun/android_icon/v0.2/pkg/darwin/converticon'
+  sha1 'eddd4037a46d6ed428cf5e0d4dc4a55a083b3827'
 
   def install
     bin.install 'converticon'
