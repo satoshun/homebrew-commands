fe9dad4954f1bd21126e9a8f8cd1aaf543e2d736
SatoShun <shun.sato1@gmail.com>
2015-04-24 03:17:33 +0000

    gog update: v0.4.0

--- gog.rb
+++ gog.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Gog < Formula
   homepage 'https://github.com/satoshun/gog'
-  version 'v0.3.0'
-  url 'https://raw.githubusercontent.com/satoshun/gog/v0.3.0/pkg/darwin/gog'
-  sha1 '11da8fce984a4811e337f54c33b4448a69f73a96'
+  version 'v0.4.0'
+  url 'https://raw.githubusercontent.com/satoshun/gog/v0.4.0/pkg/darwin/gog'
+  sha1 'f38f3e4c396fffd7940d26247715e88ff79aa9ca'
 
   def install
     bin.install 'gog'
